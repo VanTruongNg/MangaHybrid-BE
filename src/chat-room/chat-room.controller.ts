91c248be1312c66c732fd5e0346ba8e343716ae0
VanTruongNg <truongnguyen060603@gmail.com>
2024-12-05 20:14:24 +0700

    Refactor ChatRoomController and ChatRoomService to streamline private room creation and messaging. Removed createPrivateRoom method from ChatRoomController and integrated logic into ChatRoomService. Updated WebsocketGateway to handle private messaging more efficiently, including room updates and improved error handling. Enhanced socket initialization to include user private rooms and public messages.

--- src/chat-room/chat-room.controller.ts
+++ src/chat-room/chat-room.controller.ts
@@ -1,31 +1,10 @@
-import { Controller, HttpStatus, HttpException, Post, Req, Body, Param, Get } from '@nestjs/common';
+import { Controller, Req, Param, Get } from '@nestjs/common';
 import { ChatRoomService } from './chat-room.service';
 import { Auth } from 'src/auth/decorators/auth.decorator';
 
 @Controller('chat-room')
 export class ChatRoomController {
     constructor(private readonly chatRoomService: ChatRoomService) {}
-
-    @Post('private')
-    @Auth({ requireVerified: true })
-    async createPrivateRoom(
-        @Req() req: any,
-        @Body('participants') participants: string[]
-    ) {
-        if (!participants?.length) {
-            throw new HttpException('participants is required', HttpStatus.BAD_REQUEST);
-        }
-
-        if (!participants.includes(req.user._id)) {
-            participants.push(req.user);
-        }
-
-        const room = await this.chatRoomService.createPrivateRoom(participants);
-        return {
-            success: true,
-            data: room
-        };
-    }
 
     @Get('private')
     @Auth({ requireVerified: true })
